fd7fc38f4c4a915e025e942048fc826fe0df61c6
Mike <miked7531@gmail.com>
2020-07-23 12:53:39 -0400

    pg . 176
    inheritance

---
+++ parent_child.py
@@ -0,0 +1,13 @@
+class User:
+    name = 'No Name Provided'
+    email = ' '
+    password = 'zxc098'
+    account_number = 0
+
+class Patient:
+    base_charge = 10.00
+    department = 'Outpatient'
+
+class Staff:
+    base_pay = 20.00
+    department = 'Frank''s Hospital'
